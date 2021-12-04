747d7d8d323463d96a898997a4bb996418dacb4d
Brian Nguyen <whizman13@yahoo.com>
2021-12-04 16:20:35 -0600

    final changes

--- server/controllers/mainController.js
+++ server/controllers/mainController.js
@@ -12,7 +12,7 @@ module.exports = {
             }
         };
 
-        axios.get(`${baseUrl}&sort_by=${sortBy}&location="${location}""`, config)
+        axios.get(`${baseUrl}&sort_by=${sortBy}&location=${location}`, config)
             .then( restaurants => {
                 res.status(200).send(restaurants.data.businesses)})
             .catch(err => console.log(err))
